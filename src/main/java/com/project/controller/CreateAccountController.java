cc6bc97b28fafa009b968386045e68cd8a16bbcb
Hari0515 <haribabun27@gmail.com>
2025-06-09 14:19:27 +0530

    Final commit

--- src/main/java/com/project/controller/CreateAccountController.java
+++ src/main/java/com/project/controller/CreateAccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CreateAccountController {
 	
 	@GetMapping("/createaccount")
-	public String showCreateAccount() {
+	public String showCreateAccount() {    // create account controller
 		
 		return "CreateAccount";
 	}
