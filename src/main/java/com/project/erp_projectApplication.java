8de08d1e50b31971f76516cec9f6c9eee9ef8f21
Hari0515 <haribabun27@gmail.com>
2025-06-09 14:11:59 +0530

    commit

--- src/main/java/com/project/erp_projectApplication.java
+++ src/main/java/com/project/erp_projectApplication.java
@@ -2,9 +2,16 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class erp_projectApplication {
+public class erp_projectApplication extends SpringBootServletInitializer{
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(erp_projectApplication.class);
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(erp_projectApplication.class, args);
