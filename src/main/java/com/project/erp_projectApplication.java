c62709cb825cf6ea7fa11ce0fb248e5c7bd3ce2c
Hari0515 <haribabun27@gmail.com>
2025-06-10 15:17:46 +0530

    Commit

--- src/main/java/com/project/erp_projectApplication.java
+++ src/main/java/com/project/erp_projectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class erp_projectApplication {
    
 	public static void main(String[] args) {
-		SpringApplication.run(erp_projectApplication.class, args); //Main application
+		SpringApplication.run(erp_projectApplication.class, args); //Main 
 	}
 
 }
