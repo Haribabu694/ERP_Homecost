87c12d6424c7279c50ab93e355620b39d5239a7f
Hari0515 <haribabun27@gmail.com>
2025-06-11 15:10:31 +0530

    ERP

--- src/main/java/com/project/erp_projectApplication.java
+++ src/main/java/com/project/erp_projectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class erp_projectApplication {
-   
+       
 	public static void main(String[] args) {
 		SpringApplication.run(erp_projectApplication.class, args); //Main 
 	}
